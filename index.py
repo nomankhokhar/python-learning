0467247985ab8b6cd0bd1400c9228faf85135c05
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2023-10-23 04:39:07 +0500

    All zeros in the end

--- index.py
+++ index.py
@@ -9,3 +9,21 @@ I am Python Learning LOL Best in the Developer it's my though but it's may be or
 """
 
 print(comment)
+
+arr = [1, 0, 2, 0, 4665, 0, 35, 4, 0, 64, 6, 45, 75675, 675655]
+print('Orignal Arr : ', arr)
+
+index = 0
+
+for i in range(len(arr)):
+    if arr[i] != 0:
+        arr[index] = arr[i]
+        index = index + 1
+
+
+print(index)
+
+for index in range(len(arr)):
+    arr[index] = 0
+
+print("All Zero in the End : ", index)
