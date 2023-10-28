4926ea598ac34f5021c0a4fb9e716a8296bc94e7
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2023-10-29 01:42:15 +0500

    Formate Strings

--- index.py
+++ index.py
@@ -5,12 +5,25 @@ rating = 4.99
 is_published = True
 course_name = "Python Programming"
 
-print(Student_count)
+# print(Student_count)
 
 # linter check Syntax Error
 
 comment = """
-I am Python Learning LOL Best in the Developer. It's my though but it's may be or may not be true
+I am Python Learning LOLs Best in the Developer. It's my though but it's may be or may not be true
 """
 
-print(comment * 10)
+
+print("Start from start : ", comment[1])
+print("Start from End : ", comment[-3])
+print("Slice From 2 to 100 :  ", comment[2:])
+
+# Quote in Code be Like
+
+# \'
+# \"
+# \n
+# \\
+course = "Python \'Course\' with me"
+
+print(f"Noman is Learning a {course}")
