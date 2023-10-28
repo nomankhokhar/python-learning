21ac5725fc784e6e903a791638fe4f146f2252cd
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2023-10-29 02:19:23 +0500

    Conditions

--- index.py
+++ index.py
@@ -1,47 +1,105 @@
-# Variables
+# # Variables
 
-Student_count = 1000
-rating = 4.99
-is_published = True
-course_name = "Python Programming"
+# import math
+# Student_count = 1000
+# rating = 4.99
+# is_published = True
+# course_name = "Python Programming"
 
-# print(Student_count)
+# # print(Student_count)
 
-# linter check Syntax Error
+# # linter check Syntax Error
 
-comment = """
-I am Python Learning LOLs Best in the Developer. It's my though but it's may be or may not be true
-"""
-
-
-print("Start from start : ", comment[1])
-print("Start from End : ", comment[-3])
-print("Slice From 2 to 100 :  ", comment[2:])
-
-# Quote in Code be Like
-
-# \'
-# \"
-# \n
-# \\
+# comment = """
+# I am Python Learning LOLs Best in the Developer. It's my though but it's may be or may not be true
+# """
 
 
-# Strings Method's
-course = "   Python \'Course\' with me    "
+# print("Start from start : ", comment[1])
+# print("Start from End : ", comment[-3])
+# print("Slice From 2 to 100 :  ", comment[2:])
 
-print(course.upper())
-print(course.lower())
-print(course.title())
-print(course.strip())
-print(course.rstrip())
-print(course.lstrip())
+# # Quote in Code be Like
 
-
-# If find then return Index otherWise -1
-print(course.find("me"))   # return Index
-print(course.replace("P", 'j'))
-print("swift" not in course)  # Return Boolean Value
-print("me" in course)  # Return Boolean Value
+# # \'
+# # \"
+# # \n
+# # \\
 
 
-# Number's in Python
+# # Strings Method's
+# course = "   Python \'Course\' with me    "
+
+# print(course.upper())
+# print(course.lower())
+# print(course.title())
+# print(course.strip())
+# print(course.rstrip())
+# print(course.lstrip())
+
+
+# # If find then return Index otherWise -1
+# print(course.find("me"))   # return Index
+# print(course.replace("P", 'j'))
+# print("swift" not in course)  # Return Boolean Value
+# print("me" in course)  # Return Boolean Value
+
+
+# # Number's in Python
+
+# print(10 + 3)
+# print(10 - 3)
+# print(10 * 3)
+# print(10 / 3)
+# print(10 // 3)
+# print(10 % 3)
+# print(10 ** 3)
+
+
+# print(round(2.6))
+# print(abs(-2.6))
+
+
+# print(math.ceil(2.2))
+# print(math.floor(2.2))
+
+# # Type Conversion's
+
+
+# x = input("X : ")
+
+# # int()
+# # float()
+# # bool()
+# # str()
+
+# y = int(x) + 10
+
+# print(y)
+# print(type(y))
+
+# # These values consider False Values
+
+# # ""
+# # 0
+# # None
+
+
+# # String number boolean
+# # number can int float
+
+# print(bool(""))
+# print(bool(0))
+# print(bool(None))
+# print(bool(100))
+
+
+# print('\n')
+
+# print(10 > 1)
+# print(10 >= 1)
+# print(10 < 1)
+# print(10 <= 1)
+# print(10 != 1)
+# print(10 == 10)
+# print(10 != "10")
