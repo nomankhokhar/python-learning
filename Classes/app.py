6d22234011c3d0c39a3bedc456d2c0dd750f15ee
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2023-10-31 01:05:49 +0500

    Dic Class Done

--- Classes/app.py
+++ Classes/app.py
@@ -64,3 +64,30 @@
 
 
 # Another Class
+
+
+class TagCloud:
+    def __init__(self):
+        self.__tags = {}
+
+    def add(self, tag):
+        self.__tags[tag.lower()] = self.__tags.get(tag.lower(), 0) + 1
+
+    def __getitem__(self, tag):
+        return self.__tags.get(tag.lower(), 0)
+
+    def __setitem__(self, tag, count):
+        self.__tags[tag.lower()] = count
+
+    def __len__(self):
+        return len(self.__tags)
+
+    def __iter__(self):
+        return iter(self.__tags)
+
+
+cloud = TagCloud()
+print(cloud["python"])
+cloud.add("python")
+cloud["python"] = 4
+print(cloud["python"])
