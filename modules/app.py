576380fc708dd40aa4ad427e5b644671f207dc3c
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2023-11-05 04:13:45 +0500

    Sub Directries

--- modules/app.py
+++ modules/app.py
@@ -1,4 +1,13 @@
 from sales import  calc_shipping , calc_tax
+# from sales import  *
+# from sales
+
+# Above is all method to import thing from another class
+
 
 calc_shipping()
 calc_tax()
+
+import sys
+
+print(sys.path)
