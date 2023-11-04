3896872bf5eae4ab3458b962a864c25a07ca1787
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2023-11-05 04:23:51 +0500

    Module and Sub Module is done

--- modules/app.py
+++ modules/app.py
@@ -1,13 +1,28 @@
-from sales import  calc_shipping , calc_tax
-# from sales import  *
-# from sales
+# from sales import  calc_shipping , calc_tax
+# # from sales import  *
+# # from sales
 
-# Above is all method to import thing from another class
+# # Above is all method to import thing from another class
 
 
-calc_shipping()
-calc_tax()
+# calc_shipping()
+# calc_tax()
 
-import sys
+# import sys
 
-print(sys.path)
+# print(sys.path)
+
+
+
+
+
+# # in a folder import will be look like this
+# from ecommerece.sales import  calc_shipping , calc_tax
+# # from ecommerece import  sales
+
+# # The line `from ecommerece import sales` is importing the `sales` module from the `ecommerece`
+# # package. This allows you to access the functions and variables defined in the `sales` module using
+# # the `sales` prefix.
+
+# calc_shipping()
+# calc_tax()
