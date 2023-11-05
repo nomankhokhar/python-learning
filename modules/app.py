2b34a4086ed5bfabb906f605a82cfe82eac9e3d1
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2023-11-05 05:13:27 +0500

    Remove Comment's in All Code

--- modules/app.py
+++ modules/app.py
@@ -16,16 +16,16 @@
 
 
 
-# # in a folder import will be look like this
-# from ecommerece.sales import  calc_shipping , calc_tax
-# # from ecommerece import  sales
+# in a folder import will be look like this
+from ecommerece.sales import  calc_shipping , calc_tax
+# from ecommerece import  sales
 
-# # The line `from ecommerece import sales` is importing the `sales` module from the `ecommerece`
-# # package. This allows you to access the functions and variables defined in the `sales` module using
-# # the `sales` prefix.
+# The line `from ecommerece import sales` is importing the `sales` module from the `ecommerece`
+# package. This allows you to access the functions and variables defined in the `sales` module using
+# the `sales` prefix.
 
-# calc_shipping()
-# calc_tax()
+calc_shipping()
+calc_tax()
 
 
-# print(dir())
+print(dir())
