76eb73a14bda491125db6d9e7be75a3e3045fda1
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2023-11-10 04:47:12 +0500

    Password Generator

--- packages/app.py
+++ packages/app.py
@@ -150,3 +150,14 @@ from pathlib import Path
 # print(dt2 > dt1)
 
 
+
+# Working with Random Values
+
+import random
+import string
+
+print(random.random())
+print(random.randint(1,10))
+print(random.choice([1, 2, 3, 4]))
+print(random.choices([1, 2, 3, 4] , k = 2))
+print("".join(random.choices(string.ascii_letters + string.digits , k = 4)))
