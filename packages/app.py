af732220265065011888f10abc2c70b3200ac09b
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2023-11-10 04:37:41 +0500

    Time in Python

--- packages/app.py
+++ packages/app.py
@@ -119,16 +119,34 @@ from pathlib import Path
     
 
 
-# Working with timeStamp
+# # Working with timeStamp
 
-import time
+# import time
 
-def send_emails():
-    for i in range(10000):
-        pass
+# def send_emails():
+#     for i in range(10000):
+#         pass
     
-start = time.time()
-send_emails()
-end = time.time()
-duration = end - start
-print(duration)
+# start = time.time()
+# send_emails()
+# end = time.time()
+# duration = end - start
+# print(duration)
+
+
+# # Working with datetime
+
+# from datetime import datetime
+# import time
+
+# dt1 = datetime(2018, 1, 1)
+# dt2= datetime.now()
+# dt = datetime.strptime("2018/01/01", "%Y/%m/%d")
+# dt = datetime.fromtimestamp(time.time())
+# print(dt)
+
+# print(f"{dt.year}/{dt.month}/{dt.day}")
+# print(dt.strftime("%Y/%m/%d"))
+# print(dt2 > dt1)
+
+
